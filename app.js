cd9808c16c584e71de23bfd6222770fb37ae4857
Ben Langham <langham1291@outlook.com>
2020-03-18 11:56:33 +0000

    added helmet

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-// const cors = require("cors");
+const helmet = require("helmet");
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors());
+app.use(helmet());
 
 app.get("*", (req, res) => {
   res.send("I am alive").status(200);
