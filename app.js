504c5f60fb1e846d78494cf803e0e0a4f12ace98
Ben Langham <langham1291@outlook.com>
2020-05-01 19:12:34 +0100

    changed sockets

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const helmet = require("helmet");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
+
+let clients = {};
 
 app.use(helmet());
 
@@ -11,14 +13,25 @@ app.get("*", (req, res) => {
   res.send("I am alive").status(200);
 });
 
-io.on("connection", socket => {
-  console.log("connected");
-  socket.on("outgoing", x => {
+io.on("connection", (socket, room) => {
+  console.log("connected: " + socket.id);
+  socket.join("artroom");
+  if (clients["art room"] == undefined) {
+    clients["art room"] = 1;
+  } else {
+    clients["art room"]++;
+  }
+  console.log(clients);
+  io.sockets.emit("clientsJoined", clients["art room"]);
+  socket.on("outgoing", (x) => {
     console.log("x data " + x);
     io.emit("three", x);
   });
 
-  socket.on("disconnect", () => console.log("disconnected"));
+  socket.on("disconnect", () => {
+    console.log("disconnected: " + socket.id), clients["art room"]--;
+    io.sockets.emit("clientsLeft", clients["art room"]);
+  });
 });
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}!`));
