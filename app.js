63aca07ee58d3b8f6c5930c2cdf643e0d6d15d05
blanghamm <langham1291@outlook.com>
2020-05-27 14:49:22 +0100

    updated userAction to correspond correctly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const helmet = require("helmet");
 const PORT = process.env.PORT || 3005;
-const { userJoin, userLeave, users } = require("./utils/users");
+const { userJoin, userLeave, users, userAction } = require("./utils/users");
 
 app.use(helmet());
 
@@ -15,6 +15,7 @@ app.get("*", (req, res) => {
 let test = users;
 
 io.on("connection", (socket) => {
+  //Joining a room
   socket.on("subscribe", (room) => {
     if (room === "control") {
       const user = userJoin(socket.id, room);
@@ -27,6 +28,13 @@ io.on("connection", (socket) => {
   io.emit("updateOnLoad", test);
 
   console.log("join log ", test);
+
+  //User Inputs
+
+  socket.on("spawn", (input) => {
+    userAction(socket.id, input);
+    io.emit("actionId", test);
+  });
 
   socket.on("disconnect", () => {
     userLeave(socket.id);
--- utils/users.js
+++ utils/users.js
@@ -1,13 +1,28 @@
 const users = [];
 
 function userJoin(id, room) {
-  const x = (users.length % 30) * 40.05;
-  const y = (users.length / 30) * 1.05;
-  const z = 0;
+  let x = (users.length % 30) * 25.05;
+  let y = (users.length / 30) * 25.05;
+  let z = 0;
+  // if ((users.length === 4, 8, 16)) {
+  //   y = y - 40.05;
+  //   x = 0;
+  // }
+
   const user = { id, room, x, y, z };
 
   users.push(user);
   return user;
+}
+
+function userAction(id, input) {
+  users.map(function (entry, i) {
+    const index = users.findIndex((user) => user.id === id);
+    if (index === i) {
+      entry.input = input;
+    }
+    return users;
+  });
 }
 
 function userLeave(id) {
@@ -17,4 +32,4 @@ function userLeave(id) {
   }
 }
 
-module.exports = { userJoin, userLeave, users };
+module.exports = { userJoin, userLeave, users, userAction };
